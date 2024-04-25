4319802a530711d345c992ece5168567e1ff20be
satyam keshri <168067147+satyamkeshri11@users.noreply.github.com>
2024-04-25 14:44:47 +0000

    datatype

---
+++ 01_basics/01_variableconst.js
@@ -0,0 +1,14 @@
+console.log("satyam")
+const accountId = 125497
+let accountname = "satyam"
+var accountpassword = 22004466
+
+/*prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+accountname = 'keshri'
+
+console.table([accountId,accountname,accountpassword])
+// console.log(accountId);
+// console.log(accountname);
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,30 @@
+"use strict" //treat all JS code as newer version
+
+// alert(3+3) //we are using node JS not browser
+
+console.log(3+3);
+
+console.log('satyam');
+
+//code readability should be high
+
+let name = 'shiv'
+let age = 20
+let isloggedin = true
+let state;
+
+//number => 2 to the power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value / empty value
+//undefine => not empty 
+//symbol => unique value ka liye use hota ha 
+
+//object
+
+console.log(typeof "satyam");
+console.log(age,typeof age);
+
+console.log(typeof null); //It is object type
+console.log(typeof undefined);  //it is undefined type
