bf05c349fd20500997e3f9a6e13dbbba8ebabf8d
satyam keshri <168067147+satyamkeshri11@users.noreply.github.com>
2024-05-16 07:35:43 +0000

    datatype summary

--- 01_basics/datatype_summary.js
+++ 01_basics/datatype_summary.js
@@ -8,3 +8,31 @@ const scoreValue = 100.3;
 const IsLoggedIn = false;
 const OutSideTemp = null;
 let userEmail;
+
+const id = Symbol("123");
+const otherid = Symbol("123");
+
+console.log(id == otherid);
+
+const bignumber = 2354751454548225456125n;
+
+// reference (non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["Shaktiman", "naagraj", "doga"]
+
+
+let myobj = {
+    name : "satyam",
+    age : 20
+}
+
+const myfunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof OutSideTemp); // object
+console.log(typeof scoreValue); //number
+console.log(typeof myfunction); //function
+console.log(typeof heros); //object
