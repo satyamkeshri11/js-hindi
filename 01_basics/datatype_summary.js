7737f42cdd8345646136c423cc79300d99d087c9
satyam keshri <168067147+satyamkeshri11@users.noreply.github.com>
2024-05-16 07:12:46 +0000

    datatype summary

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,10 @@
+// primitive
+
+// 7 types : string, number, Boolearn, Null, Undefined, symbol, Bigint.
+
+const score = 100;
+const scoreValue = 100.3;
+
+const IsLoggedIn = false;
+const OutSideTemp = null;
+let userEmail;
